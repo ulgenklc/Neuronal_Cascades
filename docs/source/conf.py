d5bf311ba61c6f7b43ff892476ae79d2d234e0b3
Ülgen Kılıç <41899160+ulgenklc@users.noreply.github.com>
2023-02-09 16:06:22 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../docs/source/'))
+sys.path.insert(0, os.path.abspath('../docs/source'))
 
 # -- Project information -----------------------------------------------------
 
